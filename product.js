1f038ee11fd58e111e4bce5a9bbca3a9678b1e0d
Syukron Rizqi Ardhi <syuria@mt-syukronrizqi.multipolar.com>
2017-02-28 18:43:02 +0700

    Committer: Syukron Rizqi Ardhi <syuria@mt-syukronrizqi.multipolar.com> 	new file:   daily.js 	modified:   product.js

--- product.js
+++ product.js
@@ -31,6 +31,34 @@ PRODUCT_ROUTER.prototype.handleRoutes= function(router,connection) {
 			}
 	    });
 	});
+
+	router.get("/product/:uid/:tanggal",function(req,res){
+    	var data = {"error":true,
+			    "error_msg":""};
+
+        var query = `SELECT pr.kode_laporan,pr.uid,pr.tanggal,p.nama_product,pr.volume
+        			FROM product_report pr 
+        			LEFT JOIN product p ON pr.kode_product = p.kode_product  
+        			WHERE pr.uid = ? AND pr.tanggal = ?`;
+        var table = [req.params.uid,req.params.tanggal];
+        query = mysql.format(query,table);
+        connection.query(query,function(err,rows){
+            if(err) {
+                res.json({"error" : true, "error_msg" : "Error executing MySQL query"});
+            } else {
+            	console.log(rows[0].tanggal);
+                if(rows.length > 0){
+                	data["error"] = false;
+			        data["error_msg"] = 'Success..';
+			        data["history"] = rows;
+			        res.json(data);
+			    }else{
+		            data["error_msg"] = 'No History Found..';
+		            res.json(data);
+		        }
+            }
+        });
+    });
 }
 
 module.exports = PRODUCT_ROUTER;
