6048e7d2d95db4f169bd3635d814aebc854ab85a
Syukron Rizqi Ardhi <syuria@mt-syukronrizqi.multipolar.com>
2017-03-03 13:10:13 +0700

    Update Product Report

--- product.js
+++ product.js
@@ -15,7 +15,8 @@ PRODUCT_ROUTER.prototype.handleRoutes= function(router,connection) {
 			    "error_msg":""};
 
         var query = `SELECT * FROM product WHERE kode_product NOT IN
-        			(SELECT kode_product FROM product_report where uid = ? AND tanggal = NOW())`;
+        			(SELECT kode_product FROM product_report where uid = ?
+        			 AND tanggal = DATE(CONVERT_TZ(CURDATE(),@@session.time_zone,'+07:00')))`;
 	    var table = [req.params.uid];
 	    query = mysql.format(query,table);
 	    connection.query(query,function(err,rows){
@@ -38,55 +39,40 @@ PRODUCT_ROUTER.prototype.handleRoutes= function(router,connection) {
 	router.post("/products/report",function(req,res){
     	var data = {"error":true,
 			    	"error_msg":""};
-		var prods = req.body.products;
-		console.log(prods);
-		var tanggal = moment().format('YYYY-MM-DD');
-		/*var jsonObj = {
-		        "log": [{
-		            "uid": "SPG-0001",
-		            "kode_product": "PRD-0001",
-		            "volume": 95.0
-		        }, {
-		            "date": "15/09/2016",
-		            "uid": "SPG-0001",
-		            "kode_product": "PRD-0001",
-		            "volume": 95.0
-		        }, {
-		            "date": "15/09/2016",
-		            "uid": "SPG-0001",
-		            "kode_product": "PRD-0001",
-		            "volume": 95.0
-		        }]
-		    };*/
-		var jsonObj = JSON.parse(prods);
+		
+		if (isset(req.body.uid) && isset(req.body.products)) {
+	        var prods = req.body.products;
+			var tanggal = moment().format('YYYY-MM-DD');
+			
+			var jsonObj = JSON.parse(prods);
+			var jsonArr = jsonObj['products'];
+			var inserts = [];
 
-
-		 var jsonArr = jsonObj['log'];
-		 var inserts = [];
-
-		 for(var i in jsonArr){
-		 	  var uid = jsonArr[i]['uid'];
-		 	  var kode_product = jsonArr[i]['kode_product'];
-		 	  var volume = jsonArr[i]['volume'];
-		 	  inserts.push([uid, tanggal, kode_product, volume]);
-		 }
-
-        var query = `INSERT INTO product_report 
-        			(uid, tanggal, kode_product, volume) 
-					VALUES ?`;
-		var table = [inserts];
-		query = mysql.format(query,table);
-		console.log(query);
-		connection.query(query,function(err,results){
-		    if(err) {
-		    	console.log(err);
-		        res.json({"error" : true, "error_msg" : "Error executing MySQL query"});
-		    } else {
-		        data["error"] = false;
-				data["error_msg"] = 'Report succesfuly submited';
-				res.json(data);
+			for(var i in jsonArr){
+			  var kode_product = jsonArr[i]['kode_product'];
+			  var volume = jsonArr[i]['volume'];
+			  inserts.push([req.body.uid, tanggal, kode_product, volume]);
 			}
-		});
+
+	        var query = `INSERT INTO product_report 
+	        			(uid, tanggal, kode_product, volume) 
+						VALUES ?`;
+			var table = [inserts];
+			query = mysql.format(query,table);
+			connection.query(query,function(err,results){
+			    if(err) {
+			    	console.log(err);
+			        res.json({"error" : true, "error_msg" : "Error executing MySQL query"});
+			    } else {
+			        data["error"] = false;
+					data["error_msg"] = 'Report succesfuly submited';
+					res.json(data);
+				}
+			});
+		}else{
+			data["error_msg"] = 'Missing some params..';
+	        res.json(data);
+		}
 	});
 
 	router.get("/product/:uid/:tanggal",function(req,res){
